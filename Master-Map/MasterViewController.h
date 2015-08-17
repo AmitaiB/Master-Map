ea3c4cab4a8cbc09a03c869a39698e7061ce5e3e
AmitaiB <blickstein@gmail.com>
2015-08-17 07:49:57 -0400

    Initial commit

---
+++ Master-Map/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Master-Map
+//
+//  Created by Amitai Blickstein on 8/17/15.
+//  Copyright (c) 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
